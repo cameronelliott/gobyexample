9b2963ab6c11f992379cdfea8b57c94a8f44ee02
Cameron Elliott <868689+cameronelliott@users.noreply.github.com>
2021-07-15 13:17:09 -0700

    hello world example

---
+++ examples/hello-world/hello-world.js
@@ -0,0 +1,3 @@
+
+// program to solve quadratic equation
+console.log('hello world')
--- examples/hello-world/hello-world.sh
+++ examples/hello-world/hello-world.sh
@@ -1,17 +1,11 @@
 # To run the program, put the code in `hello-world.go` and
+# cam comment
 # use `go run`.
-$ go run hello-world.go
-hello world
 
-# Sometimes we'll want to build our programs into
-# binaries. We can do this using `go build`.
-$ go build hello-world.go
-$ ls
-hello-world	hello-world.go
+> console.log('hello world')
+  hello world
+< undefined
 
-# We can then execute the built binary directly.
-$ ./hello-world
-hello world
 
-# Now that we can run and build basic Go programs, let's
-# learn more about the language.
+
+
